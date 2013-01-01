6ff1e6f8b57735eaf7f54eb2cea857361d14379e
Brenton 'B-Train' Fletcher <i@bloople.net>
2013-01-02 01:23:26 +1100

    Our very own executable

---
+++ extensions.rb
@@ -0,0 +1,23 @@
+def etags_file_extensions
+  lines = `etags --list-maps`.split("\n").map { |line| line.split }
+  lines.each { |line| line.shift }
+  lines.each do |types|
+    types.map! do |type|
+      `exrex.py '#{type.gsub("+", "\\+").gsub(".", "\\.").gsub(/^\*/, '')}'`.split("\n")
+    end
+  end
+  extensions = lines.flatten
+  extensions.each do |extension|
+    extension.insert(0, "%") if extension[0, 1] == "."
+  end
+  extensions
+end
+
+def rtags_file_extensions
+  ["%.rb", "Gemfile"]
+end
+
+#Add extensions, starting with least specific and moving to more specific
+add_extensions("ctags", "-xu --format=1", etags_file_extensions, "sudo apt-get install etags or similar; see also http://ctags.sourceforge.net.")
+add_extensions("rtags", "-x -B", rtags_file_extensions, "gem install bloopletech-rtags; see also https://github.com/bloopletech/rtags.")
+
--- generate_makefile.rb
+++ generate_makefile.rb
@@ -1,38 +1,36 @@
 
+COMMANDS = {}
 EXTENSIONS = {}
-def add_extensions(exts, tool)
-  exts.each do |extension|
-    EXTENSIONS[extension] = tool
+def add_extensions(command, arguments, extensions, source)
+  COMMANDS[command] = { :arguments => arguments, :source => source }
+  extensions.each do |extension|
+    EXTENSIONS[extension] = command
   end
 end
 
-
 require_relative 'extensions.rb'
+
+COMMANDS["ag"] = { :source => "Download source from https://github.com/ggreer/the_silver_searcher and then follow the build instructions in the README.md document." }
+COMMANDS["perl"] = { :source => "Your system should already have this; try sudo apt-get install perl or similar. See also http://www.perl.org/." }
+COMMANDS["sed"] = { :source => "Your system should already have this; try sudo apt-get install sed or similar. See also http://www.gnu.org/software/sed/." }
 
 
 f = File.new("Makefile", "w")
 
-f << <<-EOF
-#Dependencies:
-# perl, whatever version your system has is fine
-# ag, https://github.com/ggreer/the_silver_searcher
-# ctags, http://ctags.sourceforge.net/, 'sudo apt-get install etags' should work
-# rtags, https://github.com/bloopletech/rtags
-
-EOF
-
-EXTENSIONS.each_pair do |extension, tool|
+EXTENSIONS.each_pair do |extension, command|
   f << "vpath #{extension} $(srcdir)\n"
 end
 
-f << <<-EOF2
+f << <<-EOF
 
-all : files.list files.tags files.mak tags.tags
+btags : files.list files.tags files.mak tags.tags 
+
+.PHONY : btags
 
 files.tags : files.list
 \tperl -pe 's/^(.*)$$/\\1\\tpath\\t\\1\\t1/g' < $< > $@
 
-files.list :
+files.list : 
 \tcd $(srcdir) && ag --search-files --nocolor --ignore '*.tags' -L '.{1000,}' | xclude_long_files > $(abspath $@)
 
 files.mak : files.list
@@ -42,12 +40,26 @@ files.mak : files.list
 .DEFAULT : 
 	touch $<
 
+check_commands : 
+EOF
+
+COMMANDS.each_pair do |command, attributes|
+  f << <<-EOE3
+\tif [ "$$(which "#{command}")" = "" ]; then \\
+\techo "Cannot find '#{command}'. Either the command is not in your PATH, or the command isn't installed. To install '#{command}': #{attributes[:source]}" && exit 1; fi
+EOE3
+end
+
+f << <<-EOF2
+
+.PHONY : check_commands
+
 EOF2
 
-EXTENSIONS.each_pair do |extension, tool|
+EXTENSIONS.each_pair do |extension, command|
   f << <<-EOE2
 #{extension}.tags : #{extension}
-\t#{tool} $< | perl -pe 's/^([^ ]+) +(\\d+) +([^ ]+?) +(.+?)$$/\\1\\t\\4\\t\\3\\t\\2/g' > $@
+\t#{command} #{COMMANDS[command][:arguments]} $< | perl -pe 's/^([^ ]+) +(\\d+) +([^ ]+?) +(.+?)$$/\\1\\t\\4\\t\\3\\t\\2/g' > $@
 
 EOE2
 end
@@ -56,7 +68,9 @@ f << <<-EOF3
 tags.tags : files.tags
 	cat $(filter %.tags,$^) | sed '/^$$/d' > tags.tags
 
-include files.mak
+ifeq ($(MAKECMDGOALS),btags)
+-include files.mak
+endif
 EOF3
 
 f.close
