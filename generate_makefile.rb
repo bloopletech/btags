47da9985686f932cea6f101e705fa4da14efd740
Brenton 'B-Train' Fletcher <i@bloople.net>
2013-01-01 19:00:58 +1100

    Works now

--- generate_makefile.rb
+++ generate_makefile.rb
@@ -10,39 +10,54 @@ end
 require_relative 'extensions.rb'
 
 
-def exts_file_var(extension)
-  "tags_#{extension.gsub(/[^\w]/, "_")}"
-end
-
 f = File.new("Makefile", "w")
-f << <<-EOF
 
-tags : tags.tags
+f << <<-EOF
+#Dependencies:
+# perl, whatever version your system has is fine
+# ag, https://github.com/ggreer/the_silver_searcher
+# ctags, http://ctags.sourceforge.net/, 'sudo apt-get install etags' should work
+# rtags, https://github.com/bloopletech/rtags
 
 EOF
 
-
 EXTENSIONS.each_pair do |extension, tool|
-  f << <<-EOE
-#{exts_file_var extension} := $(addsuffix .tags,$(shell find . -name '#{extension.sub("%", "*")}'))
-
-$(info $(#{exts_file_var extension}))
-
-#{extension}.tags : #{extension}
-\t#{tool} $< > $<.tags
-
-EOE
+  f << "vpath #{extension} $(srcdir)\n"
 end
 
-tags_sources_vars = EXTENSIONS.keys.map do |extension|
-  "$(#{exts_file_var extension})"
-end.join(" ")
-
 f << <<-EOF2
-tags.tags : #{tags_sources_vars}
-\tcat $^ > tags.tags
+
+all : files.list files.tags files.mak tags.tags
+
+files.tags : files.list
+\tperl -pe 's/^(.*)$$/\\1\\tpath\\t\\1\\t1/g' < $< > $@
+
+files.list :
+\tcd $(srcdir) && ag --search-files --nocolor --ignore '*.tags' -L '.{1000,}' | xclude_long_files > $(abspath $@)
+
+files.mak : files.list
+\tperl -pe 's/^(.*\\/)?(.*)$$/tags.tags : \\1 \\1\\2.tags\\n/g' < $< > $@
+\tperl -pe 's/^(.*\\/)?(.*)$$/\\1/g' < $< | sed '/^$$/d' | sort -u | perl -pe 's/^(.*)$$/\\1 : \\n\\tmkdir -p \\$$@\\n/g' >> $@
+
+.DEFAULT : 
+	touch $<
 
 EOF2
+
+EXTENSIONS.each_pair do |extension, tool|
+  f << <<-EOE2
+#{extension}.tags : #{extension}
+\t#{tool} $< | perl -pe 's/^([^ ]+) +(\\d+) +([^ ]+?) +(.+?)$$/\\1\\t\\4\\t\\3\\t\\2/g' > $@
+
+EOE2
+end
+
+f << <<-EOF3
+tags.tags : files.tags
+	cat $(filter %.tags,$^) | sed '/^$$/d' > tags.tags
+
+include files.mak
+EOF3
 
 f.close
 
