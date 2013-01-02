75e76fd996f7fa725600622a664a4620a757c881
Brenton 'B-Train' Fletcher <i@bloople.net>
2013-01-02 16:53:07 +1100

    Make directories first

--- generate_makefile.rb
+++ generate_makefile.rb
@@ -11,8 +11,6 @@ end
 require_relative 'extensions.rb'
 
 COMMANDS["ag"] = { :source => "Download source from https://github.com/ggreer/the_silver_searcher and then follow the build instructions in the README.md document." }
-COMMANDS["perl"] = { :source => "Your system should already have this; try sudo apt-get install perl or similar. See also http://www.perl.org/." }
-COMMANDS["sed"] = { :source => "Your system should already have this; try sudo apt-get install sed or similar. See also http://www.gnu.org/software/sed/." }
 
 
 f = File.new("Makefile", "w")
@@ -28,17 +26,17 @@ btags : files.list files.tags files.mak tags.tags
 .PHONY : btags
 
 files.tags : files.list
-\tperl -pe 's/^(.*)$$/\\1\\tpath\\t\\1\\t1/g' < $< > $@
+\tsed 's/^\\(.*\\)$$/\\1\\t1\\t\\1\\tpath/g' < $< > $@
 
 files.list : 
 \tcd $(srcdir) && ag --search-files --nocolor --ignore '*.tags' -L '.{1000,}' | xclude_long_files > $(abspath $@)
 
 files.mak : files.list
-\tperl -pe 's/^(.*\\/)?(.*)$$/tags.tags : \\1 \\1\\2.tags\\n/g' < $< > $@
-\tperl -pe 's/^(.*\\/)?(.*)$$/\\1/g' < $< | sed '/^$$/d' | sort -u | perl -pe 's/^(.*)$$/\\1 : \\n\\tmkdir -p \\$$@\\n/g' >> $@
+\tsed 's/^\\(.*\\/\\)\\?\\(.*\\)$$/tags.tags : \\1\\2.tags\\n/g' < $< > $@
+\tsed 's/^\\(.*\\/\\)\\?\\(.*\\)$$/\\1/g' < $< | sed '/^$$/d' | sort -u | sed 's/^\\(.*\\)$$/\\1 : \\1\\n\\tmkdir -p \\$$@\\n/g' >> $@
 
 .DEFAULT : 
-	touch $<
+	mkdir -p $(<D) && touch $<
 
 check_commands : 
 EOF
@@ -54,12 +52,14 @@ f << <<-EOF2
 
 .PHONY : check_commands
 
+.SECONDEXPANSION:
+
 EOF2
 
 EXTENSIONS.each_pair do |extension, command|
   f << <<-EOE2
-#{extension}.tags : #{extension}
-\t#{command} #{COMMANDS[command][:arguments]} $< | perl -pe 's/^([^ ]+) +(\\d+) +([^ ]+?) +(.+?)$$/\\1\\t\\4\\t\\3\\t\\2/g' > $@
+#{extension}.tags : #{extension} $$(@D)
+\t#{command} #{COMMANDS[command][:arguments]} $< > $@
 
 EOE2
 end
