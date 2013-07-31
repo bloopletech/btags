11d4c9b474a222cb07fed9c1917776d0bda8cfdc
Brenton "B-Train" Fletcher <i@bloople.net>
2013-08-01 04:04:14 +1000

    Vim tags

--- generate_btags_functions.rb
+++ generate_btags_functions.rb
@@ -80,7 +80,11 @@ f << <<-EOF
     cat "$TAGSDIR/$file.tags"
   done < <(cat "$TAGSDIR"/{#{COMMANDS.keys.join(",")}}.files) > "$TAGSDIR/tags.tags"
 
+  sed -e 's/^\\([^ ]\\+\\) \\+\\([A-Za-z ]\\+\\) \\+\\([0-9]\\+\\) \\+\\([^ ]\\+\\).*$/\\1\\t\\4\\t\\3/' -e '/^$/d' -e 's/\\x00/ /g' < "$TAGSDIR/tags.tags" > "$TAGSDIR/tags.vimtags"
+
   sed -e 's/ /\\x00/g' -e 's/^.*$/& path 1 & path/g' < "$TAGSDIR/files" >> "$TAGSDIR/tags.tags"
+
+  sed -i -e '/^$/d' "$TAGSDIR/tags.tags"
 
   rm "$TAGSDIR/files" "$TAGSDIR"/{#{COMMANDS.keys.join(",")}}.files "$TAGSDIR"/{#{COMMANDS.keys.join(",")}}.changed.files
 }
@@ -88,6 +92,6 @@ f << <<-EOF
 EOF
 
 btags = File.read("btags")
-btags.gsub!(/\n#Start btags dynamic functions\n.*#End btags dynamic functions\n/m, "\n#Start btags dynamic functions\n#{f}#End btags dynamic functions\n")
+btags.gsub!(/\n#Start btags dynamic functions\n.*#End btags dynamic functions\n/m, "\n#Start btags dynamic functions\n#{f.gsub('\\', '\\\\\\')}#End btags dynamic functions\n")
 File.open("btags", "w") { |w| w << btags }
 
